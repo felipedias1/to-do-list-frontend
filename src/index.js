e3ceb4bbb11641651006f5d205754dfd7a88060c
Felipe Dias <felipeferreiradias1@gmail.com>
2022-02-16 23:41:23 -0300

    build: add DataProvider and BrowserRouter in Index file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import DataProvider from './context/DataProvider';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <DataProvider>
+      <App />
+    </DataProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
