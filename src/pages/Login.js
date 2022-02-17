7c51c12fcbdf813008e913f0be7941cee4a819e4
Felipe Dias <felipeferreiradias1@gmail.com>
2022-02-17 01:06:38 -0300

    update: remove elements from formLogin

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Header from '../components/Header';
 import FormLogin from '../components/FormLogin';
 
 function Login() {
   return (
     <div>
-      <h1> TESTE </h1>
       <FormLogin />
-      <Header />
     </div>
   );
 }
