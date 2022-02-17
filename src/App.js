8866eb073eb1b847fe342e22d8d558fcab373400
Felipe Dias <felipeferreiradias1@gmail.com>
2022-02-16 23:17:53 -0300

    feat: add Routes in App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Tasks from './pages/Tasks';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/tasks" component={ Tasks } />
+        <Route path="/" component={ Login } />
+      </Switch>
     </div>
   );
 }
